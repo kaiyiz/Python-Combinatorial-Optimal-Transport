cd3fe60198af9e2889f6bfcc5f0b34ad5ac16582
kaiyiz <zhang_kaiyi@outlook.com>
2024-07-14 15:15:39 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='PyCOT',
+    packages=find_packages(include=['cot']),
+    version='0.1.0',
+    description='PyCOT: Python Combinatorial Optimal Transport',
+    author='Kaiyi Zhang',
+    license='MIT',
+    install_requires=["numpy==1.24", "scipy==1.11", "torch==1.13"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    )
